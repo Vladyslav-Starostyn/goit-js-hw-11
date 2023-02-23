2eb8e0e4ae44edbe74c0b224aa77adbede4e9666
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-02-23 23:59:24 +0200

    ---

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ buttonLoadMoreEl.addEventListener('click', onBtnLoadMoreClick);
 const perPage = 40;
 let page = 1;
 let name = '';
-
 buttonLoadMoreEl.style.display = 'none';
 
 function onFormSubmit(event) {
@@ -25,31 +24,31 @@ function onFormSubmit(event) {
   page = 1;
   name = inputFormEl.value.trim();
   clearInputFormE();
-  buttonLoadMoreEl.style.display = 'block';
+
   if (name === '') {
-    buttonLoadMoreEl.style.display = 'none';
-    Notiflix.Notify.failure('Please specify your search query.');
-    return;
+    return Notiflix.Notify.failure('Please specify your search query.');
   }
 
   fetchImages(name, page, perPage)
     .then(({ data }) => {
       galleryEl.insertAdjacentHTML('beforeend', onGaleryCards(data.hits));
       if (data.hits.length === 0) {
-        buttonLoadMoreEl.style.display = 'none';
-        Notiflix.Notify.failure(
+        return Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
-        return;
       }
 
       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
 
       onSimpleLightbox();
 
-      if (data.totalHits < perPage) {
-        buttonLoadMoreEl.style.display = 'none';
+      if (page > Math.floor(data.totalHits / perPage)) {
+        return Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
       }
+
+      buttonLoadMoreEl.style.display = 'block';
     })
     .catch(error => console.log(error));
 }
@@ -62,9 +61,9 @@ function onBtnLoadMoreClick() {
 
       onSimpleLightbox();
 
-      if (perPage > Math.ceil(data.totalHits / perPage)) {
+      if (page > Math.floor(data.totalHits / perPage)) {
         buttonLoadMoreEl.style.display = 'none';
-        Notiflix.Notify.warning(
+        return Notiflix.Notify.warning(
           "We're sorry, but you've reached the end of search results."
         );
       }
