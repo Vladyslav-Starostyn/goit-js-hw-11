113295094d4632d29258a6b31c80414bde8ce1a8
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-02-24 01:51:33 +0200

    ---

--- src/js/createCards.js
+++ src/js/createCards.js
@@ -25,15 +25,3 @@ export function onGaleryCards(hits) {
 
   return cards;
 }
-
-//  <a class="gallery__item" href="${largeImageURL}" onclick="return false;">
-//     <div class="photo-card" id=${id}>
-//     <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-//     <div class="info">
-//       <p class="info__item"> <b>Likes</b> ${likes} </p>
-//       <p class="info__item"> <b>Views</b> ${views} </p>
-//       <p class="info__item"> <b>Comments</b> ${comments} </p>
-//       <p class="info__item"> <b>Downloads</b> ${downloads} </p>
-//     </div>
-//   </div>
-//   </a>`
